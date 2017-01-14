4d1f1d6586a6f87fd80c12e30bee78631b1f2eb6
Ivan Carapovic <ivan.carapovic@gmail.com>
2017-01-14 22:24:59 +0100

    Ditch MediaBrowserCompat

--- app/src/main/java/com/icarapovic/metronome/activities/LibraryActivity.java
+++ app/src/main/java/com/icarapovic/metronome/activities/LibraryActivity.java
@@ -1,82 +1,26 @@
 package com.icarapovic.metronome.activities;
 
-import android.content.ComponentName;
-import android.os.RemoteException;
-import android.support.v4.media.MediaBrowserCompat;
-import android.support.v4.media.session.MediaControllerCompat;
-import android.support.v4.media.session.PlaybackStateCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
 
-import com.icarapovic.metronome.R;
-import com.icarapovic.metronome.service.MediaService;
+import java.util.List;
 
-public class LibraryActivity extends AppCompatActivity {
+import pub.devrel.easypermissions.EasyPermissions;
 
-    private static final int STATE_PAUSED = 0;
-    private static final int STATE_PLAYING = 1;
-    private int currentState;
-    private MediaBrowserCompat mMediaBrowserCompat;
-    private MediaBrowserCompat.ConnectionCallback mConnectionCallback;
-    private MediaControllerCompat mMediaControllerCompat;
-    private MediaControllerCompat.Callback mControllerCallback;
+public class LibraryActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_library);
 
-        init();
-
-        // create the media browser and attempt connecting to the service
-        mMediaBrowserCompat = new MediaBrowserCompat(this, new ComponentName(this, MediaService.class),
-                mConnectionCallback, getIntent().getExtras());
-        mMediaBrowserCompat.connect();
-
-    }
-
-    // init requirements for service connection
-    private void init() {
-
-        mControllerCallback = new MediaControllerCompat.Callback() {
-            @Override
-            public void onPlaybackStateChanged(PlaybackStateCompat state) {
-                // TODO - fill this and override other methods from callback that may be useful
-            }
-        };
-
-        mConnectionCallback = new MediaBrowserCompat.ConnectionCallback() {
-            @Override
-            public void onConnected() {
-                try {
-                    mMediaControllerCompat = new MediaControllerCompat(LibraryActivity.this, mMediaBrowserCompat.getSessionToken());
-                    mMediaControllerCompat.registerCallback(mControllerCallback);
-
-                    // TODO check what is the non-deprecated counterpart
-                    // setSupportMediaController(mMediaControllerCompat);
-
-                    Toast.makeText(LibraryActivity.this, "Service connected!", Toast.LENGTH_LONG).show();
-                } catch (RemoteException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            @Override
-            public void onConnectionFailed() {
-                Toast.makeText(LibraryActivity.this, "Service connection failed!", Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void onConnectionSuspended() {
-                Toast.makeText(LibraryActivity.this, "Service disconnected!", Toast.LENGTH_LONG).show();
-            }
-        };
     }
 
     @Override
-    protected void onDestroy() {
-        mMediaBrowserCompat.disconnect();
-        super.onDestroy();
+    public void onPermissionsGranted(int requestCode, List<String> perms) {
+
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, List<String> perms) {
+
     }
 }
--- app/src/main/java/com/icarapovic/metronome/service/MediaService.java
+++ app/src/main/java/com/icarapovic/metronome/service/MediaService.java
@@ -1,36 +1,18 @@
 package com.icarapovic.metronome.service;
 
-import android.app.PendingIntent;
+import android.app.Service;
 import android.content.BroadcastReceiver;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.graphics.BitmapFactory;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.net.Uri;
-import android.os.Bundle;
 import android.os.IBinder;
 import android.os.PowerManager;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationManagerCompat;
-import android.support.v4.media.MediaBrowserCompat;
-import android.support.v4.media.MediaBrowserServiceCompat;
-import android.support.v4.media.MediaMetadataCompat;
-import android.support.v4.media.session.MediaButtonReceiver;
-import android.support.v4.media.session.MediaSessionCompat;
-import android.support.v4.media.session.PlaybackStateCompat;
-import android.support.v7.app.NotificationCompat;
-import android.text.TextUtils;
 
-import com.icarapovic.metronome.R;
-
-import java.io.IOException;
-import java.util.List;
-
-public class MediaService extends MediaBrowserServiceCompat implements
+public class MediaService extends Service implements
         AudioManager.OnAudioFocusChangeListener,
         MediaPlayer.OnCompletionListener {
 
@@ -39,7 +21,6 @@ public class MediaService extends MediaBrowserServiceCompat implements
     private static final float VOLUME_DUCKED = 0.3f;
     private static final String TAG = "MediaService";
 
-    private MediaSessionCompat mMediaSession;
     private MediaPlayer mMediaPlayer;
     private BroadcastReceiver mHeadphonesReceiver;
 
@@ -47,7 +28,6 @@ public class MediaService extends MediaBrowserServiceCompat implements
     public void onCreate() {
         super.onCreate();
 
-        initMediaSession();
         initMediaPlayer();
         initNoisyReceiver();
     }
@@ -60,26 +40,6 @@ public class MediaService extends MediaBrowserServiceCompat implements
 
         mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
         mMediaPlayer.setVolume(VOLUME_MAX, VOLUME_MAX);
-    }
-
-    private void initMediaSession() {
-        // gets called when media control hardware buttons get pressed (volume up, down etc)
-        ComponentName mediaButtonReceiver = new ComponentName(getApplicationContext(), MediaButtonReceiver.class);
-
-        mMediaSession = new MediaSessionCompat(getApplicationContext(), TAG, mediaButtonReceiver, null);
-        mMediaSession.setCallback(mediaSessionCallback);
-
-        // flags telling that this media session reacts to media button actions (play, pause, etc)
-        mMediaSession.setFlags(
-                MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS | MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS);
-
-        Intent mediaButtonIntent = new Intent(Intent.ACTION_MEDIA_BUTTON);
-        mediaButtonIntent.setClass(this, MediaButtonReceiver.class);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, mediaButtonIntent, 0);
-        mMediaSession.setMediaButtonReceiver(pendingIntent);
-
-        mMediaSession = new MediaSessionCompat(this, TAG);
-        setSessionToken(mMediaSession.getSessionToken());
     }
 
     private void initNoisyReceiver() {
@@ -101,26 +61,7 @@ public class MediaService extends MediaBrowserServiceCompat implements
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         // Intent is sent which has an Action (Intent.getAction()) according to which some command is to be executed
-        MediaButtonReceiver.handleIntent(mMediaSession, intent);
         return super.onStartCommand(intent, flags, startId);
-    }
-
-    @Nullable
-    @Override
-    public BrowserRoot onGetRoot(@NonNull String clientPackageName, int clientUid, @Nullable Bundle rootHints) {
-        // if this app requests media root, return BrowserRoot object to browse media
-        if (TextUtils.equals(clientPackageName, getPackageName())) {
-            return new BrowserRoot(getString(R.string.app_name), null);
-        }
-
-        return null;
-    }
-
-    @Override
-    public void onLoadChildren(@NonNull String parentId, @NonNull Result<List<MediaBrowserCompat.MediaItem>> result) {
-        // TODO need to find what is to be done here... documentation is ofc non-existing
-
-        result.sendResult(null);
     }
 
     @Override
@@ -158,94 +99,6 @@ public class MediaService extends MediaBrowserServiceCompat implements
         }
     }
 
-    private MediaSessionCompat.Callback mediaSessionCallback = new MediaSessionCompat.Callback() {
-
-        @Override
-        public void onPlay() {
-            if (hasAudioFocus()) {
-                // start a media session
-                mMediaSession.setActive(true);
-                setMediaPlaybackState(PlaybackStateCompat.STATE_PLAYING);
-
-                // start playback and show ongoing notification
-                buildNotification(PlaybackStateCompat.STATE_PLAYING);
-                mMediaPlayer.start();
-            }
-        }
-
-        @Override
-        public void onPause() {
-            if (mMediaPlayer.isPlaying()) {
-                mMediaPlayer.pause();
-                setMediaPlaybackState(PlaybackStateCompat.STATE_PAUSED);
-                buildNotification(PlaybackStateCompat.STATE_PAUSED);
-            }
-        }
-
-        @Override
-        public void onPlayFromUri(Uri uri, Bundle extras) {
-            try {
-                mMediaPlayer.setDataSource(MediaService.this, uri);
-                mMediaPlayer.prepareAsync();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    };
-
-    private void buildNotification(int state) {
-        NotificationCompat.Builder builder = MediaStyleHelper.from(this, mMediaSession);
-        if (builder != null) {
-            NotificationCompat.MediaStyle mediaStyle = new NotificationCompat.MediaStyle()
-                    .setShowActionsInCompactView(0)
-                    .setMediaSession(mMediaSession.getSessionToken());
-            builder.setStyle(mediaStyle);
-
-            // TODO change small icon (notification)
-            builder.setSmallIcon(R.mipmap.ic_launcher);
-
-            if (state == PlaybackStateCompat.STATE_PAUSED) {
-                builder.addAction(new NotificationCompat.Action(
-                        android.R.drawable.ic_media_play, getString(R.string.play),
-                        MediaButtonReceiver.buildMediaButtonPendingIntent(this, PlaybackStateCompat.ACTION_PLAY_PAUSE))
-                );
-            } else {
-                builder.addAction(new NotificationCompat.Action(
-                        android.R.drawable.ic_media_pause,
-                        getString(R.string.pause),
-                        MediaButtonReceiver.buildMediaButtonPendingIntent(this, PlaybackStateCompat.ACTION_PLAY_PAUSE))
-                );
-            }
-
-            NotificationManagerCompat.from(this).notify(NOTIFICATION_ID, builder.build());
-        }
-    }
-
-    private void setMediaPlaybackState(int state) {
-        PlaybackStateCompat.Builder playbackStateBuilder = new PlaybackStateCompat.Builder();
-        if (state == PlaybackStateCompat.STATE_PLAYING) {
-            playbackStateBuilder.setActions(PlaybackStateCompat.ACTION_PLAY_PAUSE | PlaybackStateCompat.ACTION_PAUSE);
-        } else {
-            playbackStateBuilder.setActions(PlaybackStateCompat.ACTION_PLAY_PAUSE | PlaybackStateCompat.ACTION_PLAY);
-        }
-        playbackStateBuilder.setState(state, PlaybackStateCompat.PLAYBACK_POSITION_UNKNOWN, 0);
-        mMediaSession.setPlaybackState(playbackStateBuilder.build());
-    }
-
-    private void initMediaSessionMetadata() {
-
-        // TODO fill with actual metadata from playing item, item needs to be sent probably through params
-        MediaMetadataCompat.Builder metadataBuilder = new MediaMetadataCompat.Builder();
-
-        metadataBuilder.putBitmap(MediaMetadataCompat.METADATA_KEY_DISPLAY_ICON, BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher));
-        metadataBuilder.putBitmap(MediaMetadataCompat.METADATA_KEY_ALBUM_ART, BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher));
-        metadataBuilder.putBitmap(MediaMetadataCompat.METADATA_KEY_ART, BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher));
-        metadataBuilder.putString(MediaMetadataCompat.METADATA_KEY_DISPLAY_TITLE, "Such Song");
-        metadataBuilder.putString(MediaMetadataCompat.METADATA_KEY_DISPLAY_SUBTITLE, "Much Singer");
-
-        mMediaSession.setMetadata(metadataBuilder.build());
-    }
-
     private boolean hasAudioFocus() {
         AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
         int result = audioManager.requestAudioFocus(this, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
@@ -264,7 +117,12 @@ public class MediaService extends MediaBrowserServiceCompat implements
         AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
         audioManager.abandonAudioFocus(this);
         unregisterReceiver(mHeadphonesReceiver);
-        mMediaSession.release();
         NotificationManagerCompat.from(this).cancel(NOTIFICATION_ID);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
     }
 }
