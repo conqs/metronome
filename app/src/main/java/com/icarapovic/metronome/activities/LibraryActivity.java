148ccc9c99e2f4b373e2ee848203b9c3ba7a2664
Ivan Carapovic <ivan.carapovic@gmail.com>
2017-01-17 20:19:02 +0100

    Check for storage permission on startup

--- app/src/main/java/com/icarapovic/metronome/activities/LibraryActivity.java
+++ app/src/main/java/com/icarapovic/metronome/activities/LibraryActivity.java
@@ -1,7 +1,11 @@
 package com.icarapovic.metronome.activities;
 
+import android.Manifest;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.icarapovic.metronome.R;
 
 import java.util.List;
 
@@ -9,9 +13,19 @@ import pub.devrel.easypermissions.EasyPermissions;
 
 public class LibraryActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks {
 
+    private static final int REQUEST_CODE = 100;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if(!EasyPermissions.hasPermissions(this, Manifest.permission.READ_EXTERNAL_STORAGE)){
+            EasyPermissions.requestPermissions(this, getString(R.string.info_permissions_external_storage), REQUEST_CODE, Manifest.permission.READ_EXTERNAL_STORAGE);
+        }
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     @Override
