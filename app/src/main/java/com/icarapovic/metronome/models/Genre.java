3dfd9e289bec56f4a272e6740eb4a16759b7eadf
Ivan Carapovic <ivan.carapovic@gmail.com>
2017-02-07 20:50:57 +0100

    Make Genre parcelable

--- app/src/main/java/com/icarapovic/metronome/models/Genre.java
+++ app/src/main/java/com/icarapovic/metronome/models/Genre.java
@@ -1,6 +1,9 @@
 package com.icarapovic.metronome.models;
 
-public class Genre {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Genre implements Parcelable {
 
     public Genre(int genreId, String genreName) {
         mGenreId = genreId;
@@ -9,6 +12,23 @@ public class Genre {
 
     private final int mGenreId;
     private final String mGenreName;
+
+    protected Genre(Parcel in) {
+        mGenreId = in.readInt();
+        mGenreName = in.readString();
+    }
+
+    public static final Creator<Genre> CREATOR = new Creator<Genre>() {
+        @Override
+        public Genre createFromParcel(Parcel in) {
+            return new Genre(in);
+        }
+
+        @Override
+        public Genre[] newArray(int size) {
+            return new Genre[size];
+        }
+    };
 
     public int getGenreId() {
         return mGenreId;
@@ -21,5 +41,16 @@ public class Genre {
     @Override
     public String toString() {
         return "Genre [ id=" + mGenreId + ", name=" + mGenreName + " ]";
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(mGenreId);
+        dest.writeString(mGenreName);
     }
 }
